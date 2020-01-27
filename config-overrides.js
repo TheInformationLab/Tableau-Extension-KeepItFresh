eb3ff8fbd2c674aad57aa3c9430da30dc8239e46
andre347 <aedevries19@gmail.com>
2020-01-27 14:34:55 +0000

    Add React rewire libs and configure file and latest Ext API Library

---
+++ config-overrides.js
@@ -0,0 +1,28 @@
+const multipleEntry = require("react-app-rewire-multiple-entry")([
+  {
+    entry: "src/entry_index.tsx",
+    template: "public/index.html",
+    outPath: "/index.html"
+  },
+  {
+    entry: "src/entry_config.tsx",
+    template: "public/config.html",
+    outPath: "/config.html"
+  },
+  {
+    entry: "src/entry_shape.tsx",
+    template: "public/shape.html",
+    outPath: "/shape.html"
+  }
+]);
+
+module.exports = {
+  webpack: function(config, env) {
+    multipleEntry.addMultiEntry(config);
+    return config;
+  },
+  devServer: function(configFunction) {
+    multipleEntry.addEntryProxy(configFunction);
+    return configFunction;
+  }
+};
