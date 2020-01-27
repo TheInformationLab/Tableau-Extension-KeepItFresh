ac3e41fb869713b83ad21e91f38dd8f8660e0d09
andre347 <aedevries19@gmail.com>
2020-01-27 14:05:52 +0000

    added GA

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import ReactGA from "react-ga";
 import React from "react";
 import Main from "./navigation/Main.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
+
+ReactGA.initialize("UA-141023410-2");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
