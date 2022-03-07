66918ba376edc4b44c92bee011ea045607e1472b
Andre <andre.devries@theinformationlab.co.uk>
2022-03-07 17:39:18 +0000

    eslint error

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -15,7 +15,7 @@ function Home(props) {
     pausedText: "▐▐ ",
     color: "#000",
     alpha: 0.9,
-    size: 70
+    size: 70,
   });
   const [paused, setPaused] = useState(true);
 
@@ -27,33 +27,49 @@ function Home(props) {
   useEffect(() => {
     tableau.extensions.initializeAsync({ configure: configure }).then(() => {
       console.log("[Home.js] Opening...");
-      console.log(`[Home.js] checked data source list:`, checkedDataSourcesList());
+      console.log(
+        `[Home.js] checked data source list:`,
+        checkedDataSourcesList()
+      );
 
       //checking if toggle is on or off
-      let selectedUpdateToggle = tableau.extensions.settings.get("updateToggle");
+      let selectedUpdateToggle =
+        tableau.extensions.settings.get("updateToggle");
       //this is list of data sources
-      let selectedDataSources = tableau.extensions.settings.get("dataSourceList");
+      let selectedDataSources =
+        tableau.extensions.settings.get("dataSourceList");
       //this is the actually value
       let selectedStepperValue = tableau.extensions.settings.get("timeRefresh");
       // minutes or seconds
-      let selectedIntervalValue = tableau.extensions.settings.get("intervalValue");
+      let selectedIntervalValue =
+        tableau.extensions.settings.get("intervalValue");
       // find the colour from the workbook settings
       let selectedTimerColour = tableau.extensions.settings.get("timerColour");
 
       if (selectedUpdateToggle && selectedUpdateToggle !== null) {
         props.updateHandler(JSON.parse(selectedUpdateToggle));
         if (selectedDataSources && selectedDataSources !== null) {
-          console.log(`[Home.js] ${JSON.parse(selectedDataSources).length} datasources found`);
           console.log(
-            `[Home.js] Every ${JSON.parse(selectedStepperValue)} ${JSON.parse(selectedIntervalValue)} going to refresh`
+            `[Home.js] ${
+              JSON.parse(selectedDataSources).length
+            } datasources found`
           );
-          console.log(`[Home.js] Received Colour ${JSON.parse(selectedTimerColour)}`);
-          let minsOrSecs = JSON.parse(selectedIntervalValue) ? JSON.parse(selectedIntervalValue) : "Seconds";
+          console.log(
+            `[Home.js] Every ${JSON.parse(selectedStepperValue)} ${JSON.parse(
+              selectedIntervalValue
+            )} going to refresh`
+          );
+          console.log(
+            `[Home.js] Received Colour ${JSON.parse(selectedTimerColour)}`
+          );
+          let minsOrSecs = JSON.parse(selectedIntervalValue)
+            ? JSON.parse(selectedIntervalValue)
+            : "Seconds";
           setPaused(JSON.parse(selectedUpdateToggle) ? false : true);
-          setCounter(currentState => ({
+          setCounter((currentState) => ({
             ...currentState,
             seconds: intervalTime(minsOrSecs, JSON.parse(selectedStepperValue)),
-            color: JSON.parse(selectedTimerColour)
+            color: JSON.parse(selectedTimerColour),
           }));
         } else {
           console.log("[Home.js] no data sources saved!");
@@ -71,11 +87,11 @@ function Home(props) {
     const popupUrl = `${window.location.origin}/configure`;
     tableau.extensions.ui
       .displayDialogAsync(popupUrl, payload, { height: 540, width: 400 })
-      .then(closePayload => {
+      .then((closePayload) => {
         console.log("Config window closed");
         refreshSettings();
       })
-      .catch(error => {
+      .catch((error) => {
         switch (error.errorCode) {
           case tableau.ErrorCodes.DialogClosedByUser:
             console.log("[Home.js] Dialog was closed by user");
@@ -91,24 +107,25 @@ function Home(props) {
   // when the refreshtimer has finished, execute this function
   // it increments the number of completions, to make sure the component rerenders
   const newState = () => {
-    setPaused(paused => !paused);
+    setPaused((paused) => !paused);
     const getdashboard = tableau.extensions.dashboardContent.dashboard;
     let dataSourceFetchPromises = [];
     let dashboardDataSources = {};
 
-    getdashboard.worksheets.map(worksheet => {
+    // eslint-disable-next-line
+    getdashboard.worksheets.map((worksheet) => {
       dataSourceFetchPromises.push(worksheet.getDataSourcesAsync());
     });
 
-    Promise.all(dataSourceFetchPromises).then(fetchResults => {
-      fetchResults.forEach(dataSourcesForWorksheet => {
-        dataSourcesForWorksheet.forEach(dataSource => {
+    Promise.all(dataSourceFetchPromises).then((fetchResults) => {
+      fetchResults.forEach((dataSourcesForWorksheet) => {
+        dataSourcesForWorksheet.forEach((dataSource) => {
           if (!dashboardDataSources[dataSource.id]) {
             dashboardDataSources[dataSource.id] = dataSource;
           }
         });
       });
-      checkedDataSourcesList().map(item => {
+      checkedDataSourcesList().map((item) => {
         for (let dataSourceId in dashboardDataSources) {
           let name = item.data.name;
           const dataSource = dashboardDataSources[dataSourceId];
@@ -120,15 +137,15 @@ function Home(props) {
         }
       });
       setTimeout(() => {
-        setPaused(paused => !paused);
+        setPaused((paused) => !paused);
       }, 5000);
     });
 
     console.log("[Home.js] countdown finished!");
     // set the newstate with the new completions number
-    setCounter(currentState => ({
+    setCounter((currentState) => ({
       ...currentState,
-      completions: currentState.completions + 1
+      completions: currentState.completions + 1,
     }));
   };
 
@@ -136,7 +153,7 @@ function Home(props) {
     let selectedDataSources = tableau.extensions.settings.get("dataSourceList");
     if (selectedDataSources) {
       let list = JSON.parse(selectedDataSources);
-      let newList = list.filter(dsource => {
+      let newList = list.filter((dsource) => {
         return dsource.checked;
       });
       return newList;
@@ -148,7 +165,8 @@ function Home(props) {
     console.log("[Home.js] refreshing settings", props);
     let selectedStepperValue = tableau.extensions.settings.get("timeRefresh");
     let selectedUpdateToggle = tableau.extensions.settings.get("updateToggle");
-    let selectedIntervalValue = tableau.extensions.settings.get("intervalValue");
+    let selectedIntervalValue =
+      tableau.extensions.settings.get("intervalValue");
     let selectedTimerColour = tableau.extensions.settings.get("timerColour");
 
     // this sets the pause setting on the timer component
@@ -157,10 +175,13 @@ function Home(props) {
       setPaused(JSON.parse(selectedUpdateToggle) ? false : true);
     }
     if (selectedUpdateToggle && selectedIntervalValue && selectedStepperValue) {
-      setCounter(currentState => ({
+      setCounter((currentState) => ({
         ...currentState,
-        seconds: intervalTime(JSON.parse(selectedIntervalValue), JSON.parse(selectedStepperValue)),
-        color: JSON.parse(selectedTimerColour)
+        seconds: intervalTime(
+          JSON.parse(selectedIntervalValue),
+          JSON.parse(selectedStepperValue)
+        ),
+        color: JSON.parse(selectedTimerColour),
       }));
     }
   }
@@ -171,13 +192,18 @@ function Home(props) {
     //this is list of data sources
     let selectedDataSources = tableau.extensions.settings.get("dataSourceList");
     if (selectedUpdateToggle && selectedDataSources) {
-      setPaused(paused => !paused);
+      setPaused((paused) => !paused);
     }
   }
 
   return (
     <>
-      <Counter state={counter} stateHandler={newState} paused={paused} clickHandler={handleOnClick} />
+      <Counter
+        state={counter}
+        stateHandler={newState}
+        paused={paused}
+        clickHandler={handleOnClick}
+      />
     </>
   );
 }
